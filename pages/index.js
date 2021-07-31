1d4dce2149c1dd53f9f875da654e00a67c92fbc0
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-07-30 23:45:30 -0700

    copy over raw html, format it, and make it react/jsx-friendly

--- pages/index.js
+++ pages/index.js
@@ -8,62 +8,121 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="flex flex-col items-center justify-center">
+      	<div className="p-4 pb-20 overflow-x-auto">
+      		<div>
+      			<div className="hidden lg:block" style={{width: "728px", margin: "auto"}}>
+      				<a href="https://etheremura.io/?utm_source=website&amp;utm_medium=banner&amp;utm_campaign=rarity&amp;utm_content=topbanner1" target="_blank"><img src="https://ewr1.vultrobjects.com/current/etheremura2_horiz_d" className="w-full h-auto" /></a>
+      			</div>
+      			<div className="block lg:hidden" style={{width: "300px", margin: "auto"}}>
+      				<a href="https://etheremura.io/?utm_source=website&amp;utm_medium=banner&amp;utm_campaign=rarity&amp;utm_content=topbanner1" target="_blank"><img src="https://ewr1.vultrobjects.com/current/etheremura2_horiz_m" className="w-full h-auto" /> </a>
+      			</div>
+      			<div className="mt-0.5 text-sm text-center textColor600">Please visit our sponsors!</div>
+      		</div>
+      		<div className="mt-6 mb-10">
+      			<h2 className="mb-4 text-3xl font-extrabold text-center textColor700">Newest Collections Added</h2>
+      			<div className="flex flex-row justify-center m-auto" style={{maxWidth: ""}}>
+      				<div className="flex flex-row flex-wrap justify-around">
+      					<div className="flex flex-row flex-wrap justify-center">
+      						<div className="flex flex-row flex-wrap justify-center">
+      							<div className="mb-4 ml-4 overflow-hidden border border-gray-300 rounded-lg shadow-md bgCard dark:border-gray-800">
+      								<div className="w-72">
+      									<a href="/svinsfarm" className="">
+      										<div className="relative w-full overflow-hidden" style={{height: "220px"}}>
+      											<div className="absolute w-full font-bold text-center text-white bg-pink-700">Featured</div> <img src="https://projects.rarity.tools/static/images/headers/svinsfarm/header.jpg" className="object-cover object-center w-full h-full" /> </div>
+      										<div className="flex flex-row mt-2">
+      											<div className="p-2 ml-2">
+      												<div className="font-bold text-pink-600 dark:text-gray-300">SVINS</div>
+      												<div className="text-sm text-gray-400">Added 13 Jul 2021</div>
+      												<div className="text-sm text-gray-400">Owners: 213</div>
+      												<div className="text-sm text-gray-400">Total Volume: 74.23 ETH</div>
+      											</div>
+      											<div className="flex-grow"></div>
+      											<div className="mt-3 mr-3" style={{maxWidth: "3rem"}}> <img src="https://lh3.googleusercontent.com/BEsmio4587tvd8RE12v6HDUy9jSSGMPoKRx_PO3tdK-7sigUg-86OcQxfJEWZyJVV2sfWN2rhgDVPQ-T6Vs-GfpHh0V0ulvun1Yb=s120" className="w-16 h-auto rounded-full" /> </div>
+      										</div>
+      										<div className="p-4 pt-2 text-sm notes dark:text-gray-300">
+      											<p> <strong>SVINS</strong> – is the Largest <strong>NON-PROCEDURAL</strong> 😮 3D Diverse Collection of 1000 unique Limited Edition NFTs collectibles living on the Ethereum blockchain ever created, minted and attributed one by one by a single person without the help of Ai. </p>
+      											<p>Each SVIN i...</p>
+      										</div>
+      									</a>
+      								</div>
+      							</div>
+      							<div className="mb-4 ml-4 overflow-hidden border border-gray-300 rounded-lg shadow-md bgCard dark:border-gray-800">
+      								<div className="w-72">
+      									<a href="/long-neckie-ladies" className="">
+      										<div className="relative w-full overflow-hidden" style={{height: "220px"}}> <img src="https://lh3.googleusercontent.com/QLSVo3VbidUSWlWmBw9uByy7mlJfptj07LcE9VBeE9ITnuyvV4l_4Pai0BFuhHwmNF-GuPHQddvRPGpmUBFipkNmTFZoq9IJirG82S0=s2500" className="object-cover object-center w-full h-full" /> </div>
+      										<div className="flex flex-row mt-2">
+      											<div className="p-2 ml-2">
+      												<div className="font-bold text-pink-600 dark:text-gray-300">Long Neckie Ladies</div>
+      												<div className="text-sm text-gray-400">Added 30 Jul 2021</div>
+      												<div className="text-sm text-gray-400">Owners: 908</div>
+      												<div className="text-sm text-gray-400">Total Volume: 36.07 ETH</div>
+      											</div>
+      											<div className="flex-grow"></div>
+      											<div className="mt-3 mr-3" style={{maxWidth: "3rem"}}><img src="https://lh3.googleusercontent.com/eV3XbDmvPumzQMiWECbJK8P0LY3YlRYM6k4Xn5QmYMRy0yRxi5hsLJcFNxAp4JxpjpzRIwxpDdi83CdUa0nWl4BdtZTarelmG5Vu=s120" className="w-16 h-auto rounded-full" /></div>
+      										</div>
+      										<div className="p-4 pt-2 text-sm notes dark:text-gray-300">
+      											<p>Long Neckie Ladies are a fine art, hand drawn, computer generated collectible of 3333 diverse females created by 12-year-old digital artist Nyla Hayes. Each Long Neckie features an elongated neck that is inspired by Nyla’s favorite dinosaur, the bron...</p>
+      										</div>
+      									</a>
+      								</div>
+      							</div>
+      						</div>
+      						<div className="block w-full mb-4 lg:hidden">
+      							<div className="w-full m-auto my-2" style={{width: "300px"}}>
+      								<a target="_blank"><img src="https://ewr1.vultrobjects.com/current/mixedfam2_vert_m" className="w-full h-auto" /></a>
+      								<div className="text-xs text-center">Please visit our sponsors!</div>
+      							</div>
+      						</div>
+      						<div className="flex flex-row flex-wrap justify-center">
+      							<div className="mb-4 ml-4 overflow-hidden border border-gray-300 rounded-lg shadow-md bgCard dark:border-gray-800">
+      								<div className="w-72">
+      									<a href="/crazy-lizard-army" className="">
+      										<div className="relative w-full overflow-hidden" style={{height: "220px"}}> <img src="https://lh3.googleusercontent.com/cpmhZkIc9dpFXCeOxaOpXBJ7YaMIDVjUPm48LAs1Lz57DzapGVzYUIm35aTTdFCk2LpB2uYk62WlimAIX_WE3IY1LHnczB66JTLLPQ=s2500" className="object-cover object-center w-full h-full" /> </div>
+      										<div className="flex flex-row mt-2">
+      											<div className="p-2 ml-2">
+      												<div className="font-bold text-pink-600 dark:text-gray-300"> Crazy Lizard Armyl, /. </div>
+      												<div className="text-sm text-gray-400">Added 30 Jul 2021</div>
+      												<div className="text-sm text-gray-400">Owners: 1395</div>
+      												<div className="text-sm text-gray-400">Total Volume: 109.9 ETH</div>
+      											</div>
+      											<div className="flex-grow"></div>
+      											<div className="mt-3 mr-3" style={{maxWidth: "3rem"}}><img src="https://lh3.googleusercontent.com/Qg1YzUN_XOtHrX_MnilRbm29CTA-oNscS9dkVbSGKrzoxHImq5lOrON7H-COBqekKo7ZDENj-8y2Gp7bXxJkhEwid_XiI-9LgTDBzg=s120" className="w-16 h-auto rounded-full" /></div>
+      										</div>
+      										<div className="p-4 pt-2 text-sm notes dark:text-gray-300">
+      											<p>The Crazy Lizard Army is made up of 10000 unique lizards. Become soldier in our army when you hold a lizard. In our army, there are all sorts of Lizards, some are good, some are evil, and some are CRAZY! Who knows which you will meet?
+      												<br /> Did you know yo...</p>
+      										</div>
+      									</a>
+      								</div>
+      							</div>
+      							<div className="mb-4 ml-4 overflow-hidden border border-gray-300 rounded-lg shadow-md bgCard dark:border-gray-800">
+      								<div className="w-72">
+      									<a href="/pudgypenguins" className="">
+      										<div className="relative w-full overflow-hidden" style={{height: "220px"}}> <img src="https://lh3.googleusercontent.com/y2HMjE2tc2dNZVP1EuDOE3IReS2plYMUm3yq2-jVp9_9L9D3H1IVHFbmalmXCoG2KYfy_R7LYK8c4Q8QPuzv41FmppRWLpyW02ii=s2500" className="object-cover object-center w-full h-full" /> </div>
+      										<div className="flex flex-row mt-2">
+      											<div className="p-2 ml-2">
+      												<div className="font-bold text-pink-600 dark:text-gray-300"> Pudgy Penguins </div>
+      												<div className="text-sm text-gray-400">Added 29 Jul 2021</div>
+      												<div className="text-sm text-gray-400">Owners: 1824</div>
+      												<div className="text-sm text-gray-400">Total Volume: 177.83 ETH</div>
+      											</div>
+      											<div className="flex-grow"></div>
+      											<div className="mt-3 mr-3" style={{maxWidth: "3rem"}}><img src="https://lh3.googleusercontent.com/bcCd1TfusKK6wWjmshwmizmY9j7An3pp9kxopMxfIt-_I8WFnSIK-5gevOduoYK4Qpq2e3DyXgROKNfkP396W5ViEYXhxoyAZG3s_vY=s120" className="w-16 h-auto rounded-full" /></div>
+      										</div>
+      										<div className="p-4 pt-2 text-sm notes dark:text-gray-300">
+      											<p>Located in the freezing cold, arctic region of the metaverse you can find 8,888 cute chubby Pudgy Penguins sliding around on the ETH blockchain.</p>
+      											<p>Come join the huddle today at <a href="https://pudgypenguins.io">pudgypenguins.io</a></p>
+      										</div>
+      									</a>
+      								</div>
+      							</div>
+      						</div>
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
