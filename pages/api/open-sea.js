5323ac443fb9a4fb56c4500d4a35c27deaf4e527
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-08-16 00:36:06 -0700

    only cache fields that we care about

--- pages/api/open-sea.js
+++ pages/api/open-sea.js
@@ -7,14 +7,36 @@ import { sortByPath } from "../../util/sort"
 const CACHE_KEEP_TIME_S = 43200
 const API_REQUEST_LIMIT_MS = 750
 const TOTAL_REQUESTS = 10
+// const TOTAL_REQUESTS = 4
 const MAX_REQUESTS_PER_CACHE_REQUEST = 2
 const MAX_COLLECTIONS_PER_REQUEST = 250
+// const MAX_COLLECTIONS_PER_REQUEST = 200
 const TOP10_LISTS = {
   top10by7DayVol: {stat: "seven_day_volume", unit: "ETH"},
   top10byTotalVol: {stat: "total_volume", unit: "ETH"},
   top10by7DayAvgPrice: {stat: "seven_day_average_price", unit: "ETH"},
   top10byNumOwners: {stat: "num_owners", unit: "Owners"}
 }
+
+const rootFields = [
+  "banner_image_url",
+  "created_at",
+  "description",
+  "image_url",
+  "name",
+  "slug"
+]
+const statFields = [
+  "seven_day_volume",
+  "seven_day_sales",
+  "seven_day_average_price",
+  "total_supply",
+  "num_owners",
+  "owner_pct",
+  "market_cap",
+  "total_volume",
+  "total_sales"
+]
 
 const top10ByStat = (collectionArray, stat) => {
   const collectionsSubset = JSON.parse(JSON.stringify(collectionArray))
@@ -52,8 +74,11 @@ module.exports = async (req, res) => {
   let rawCollections = [];
 
   let start = Date.now();
-  let cache = await redis.get("cache")
-  cache = JSON.parse(cache)
+  // let cacheResponse =
+
+  // console.log(cache)
+  let cache = JSON.parse(await redis.get("cache"))
+
   let result = {}
 
   if (cache) {
@@ -65,7 +90,7 @@ module.exports = async (req, res) => {
   } else {
     console.log("loading from api")
 
-    redis.set("cache", "", "EX", CACHE_KEEP_TIME_S)
+    redis.set("cache", "[", "EX", CACHE_KEEP_TIME_S)
 
     // [...Array(cacheRequests).keys()].forEach(async(cReq) => {
 
@@ -77,19 +102,37 @@ module.exports = async (req, res) => {
 
       let collectionsToCache = []
 
+      let firstEntrySet = false;
       await Promise.all(openSeaRequests).then((data) => {
         data.forEach((response, i) => {
           // only care about those that have volume
-          const validCollections = response.filter(c => c.stats.total_volume > 0)
+          let validCollections = response.filter(c => c.stats.total_volume > 0)
+          // console.log(validCollections.length)
+          validCollections = validCollections.map(c => {
+            let newC = {"stats": {}};
 
+            rootFields.forEach(f => newC[f] = c[f])
+            statFields.forEach(f => newC["stats"][f] = c["stats"][f])
+
+            return newC
+          })
           rawCollections = rawCollections.concat(validCollections)
           collectionsToCache = collectionsToCache.concat(validCollections)
 
           if ((i+1) % MAX_REQUESTS_PER_CACHE_REQUEST == 0) {
-            redis.append("cache", JSON.stringify(collectionsToCache))
+            if(collectionsToCache.length){
+              if(!firstEntrySet) {
+                redis.append("cache", JSON.stringify(...collectionsToCache))
+                firstEntrySet = true
+              } else {
+                redis.append("cache", `,${JSON.stringify(...collectionsToCache)}`)
+              }
+            }
             collectionsToCache = []
           }
         })
+        redis.append("cache", "]")
+
         result.data = rawCollections
         result.type = "api"
         result.latency = Date.now() - start;
