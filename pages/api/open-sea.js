ab5de1448b6b007a0fbbff6eec8a62b6881e5dfa
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-08-19 00:48:56 -0700

    finally get caching to work properly
    - basically checks cache to see if it has anything:
    - if it is not empty, then use data from cache
    - if it is empty, call the open sea api in a loop, getting 250 collections at a time
    - filter the collections to get only those that have seven day volume and aren't called Untitled Collelctions #XXX
    - of the remaining collections add them to cache in chunks of at most 500 at a time
    - still need to write a script that wouldl go thru ALL the collections and cache those of interest
    - also need to figure out better filtering than simple filtering based on 7 day volume

--- pages/api/open-sea.js
+++ pages/api/open-sea.js
@@ -6,11 +6,9 @@ import { sortByPath } from "../../util/sort"
 
 const CACHE_KEEP_TIME_S = 43200
 const API_REQUEST_LIMIT_MS = 750
-const TOTAL_REQUESTS = 10
-// const TOTAL_REQUESTS = 4
-const MAX_REQUESTS_PER_CACHE_REQUEST = 2
+const TOTAL_REQUESTS = 200
+const MAX_COLLECTIONS_PER_CACHE_REQUEST = 500
 const MAX_COLLECTIONS_PER_REQUEST = 250
-// const MAX_COLLECTIONS_PER_REQUEST = 200
 const TOP10_LISTS = {
   top10by7DayVol: {stat: "seven_day_volume", unit: "ETH"},
   top10byTotalVol: {stat: "total_volume", unit: "ETH"},
@@ -20,7 +18,7 @@ const TOP10_LISTS = {
 
 const rootFields = [
   "banner_image_url",
-  "created_at",
+  "created_date",
   "description",
   "image_url",
   "name",
@@ -71,78 +69,79 @@ function getFromOpenSea(url, waitIndex) {
 }
 
 module.exports = async (req, res) => {
-  let rawCollections = [];
-
-  let start = Date.now();
-  // let cacheResponse =
-
-  // console.log(cache)
   let cache = JSON.parse(await redis.get("cache"))
-
+  let rawCollections = [];
   let result = {}
+  let start = Date.now();
 
   if (cache) {
      console.log("loading from cache")
      result.data = cache
-     result.type = "redis"
      result.latency = Date.now() - start;
-     // return res.status(200).json(result)
+     result.type = "redis"
   } else {
     console.log("loading from api")
 
-    redis.set("cache", "[", "EX", CACHE_KEEP_TIME_S)
+    const urls = [...Array(TOTAL_REQUESTS).keys()].map((i) => {
+      return `https://api.opensea.io/api/v1/collections?offset=${i*MAX_COLLECTIONS_PER_REQUEST}&limit=${MAX_COLLECTIONS_PER_REQUEST}`
+    })
+    const openSeaRequests = await urls.map((url, i) => getFromOpenSea(url, i));
 
-    // [...Array(cacheRequests).keys()].forEach(async(cReq) => {
+    let collectionsToCache = []
+    let firstEntrySet = false;
+    let numCached = 0;
 
-      const urls = [...Array(TOTAL_REQUESTS).keys()].map((i) => {
-        return `https://api.opensea.io/api/v1/collections?offset=${i*MAX_COLLECTIONS_PER_REQUEST}&limit=${MAX_COLLECTIONS_PER_REQUEST}`
-      })
+    await Promise.all(openSeaRequests).then((data) => {
+      for(let response of data){
+        if (!response) {
+          continue;
+        }
 
-      const openSeaRequests = await urls.map((url, i) => getFromOpenSea(url, i));
+        // only care about those that have volume > 1 ETH and aren't Untitled Collections
+        let validCollections = response.filter(c => c.stats.seven_day_volume > 1 && !c.name.startsWith("Untitled Collection"))
 
-      let collectionsToCache = []
+        validCollections = validCollections.map(c => {
+          let newC = {"stats": {}};
 
-      let firstEntrySet = false;
-      await Promise.all(openSeaRequests).then((data) => {
-        data.forEach((response, i) => {
-          // only care about those that have volume
-          let validCollections = response.filter(c => c.stats.total_volume > 0)
-          // console.log(validCollections.length)
-          validCollections = validCollections.map(c => {
-            let newC = {"stats": {}};
+          rootFields.forEach(f => newC[f] = c[f])
+          statFields.forEach(f => newC["stats"][f] = c["stats"][f])
 
-            rootFields.forEach(f => newC[f] = c[f])
-            statFields.forEach(f => newC["stats"][f] = c["stats"][f])
-
-            return newC
-          })
-          rawCollections = rawCollections.concat(validCollections)
-          collectionsToCache = collectionsToCache.concat(validCollections)
-
-          if ((i+1) % MAX_REQUESTS_PER_CACHE_REQUEST == 0) {
-            if(collectionsToCache.length){
-              if(!firstEntrySet) {
-                redis.append("cache", JSON.stringify(...collectionsToCache))
-                firstEntrySet = true
-              } else {
-                redis.append("cache", `,${JSON.stringify(...collectionsToCache)}`)
-              }
-            }
-            collectionsToCache = []
-          }
+          return newC
         })
-        redis.append("cache", "]")
+        rawCollections = rawCollections.concat(validCollections)
+      }
 
-        result.data = rawCollections
-        result.type = "api"
-        result.latency = Date.now() - start;
-        // if ()MAX_REQUESTS_PER_CACHE_REQUEST
+      result.data = rawCollections
+      result.latency = Date.now() - start;
+      result.type = "api"
+    })
 
-      })
-    // })
+    const numCollections = rawCollections.length
+    console.log(numCollections)
+    if(numCollections > 0 && numCollections <= MAX_COLLECTIONS_PER_CACHE_REQUEST) {
+      redis.set("cache", JSON.stringify(rawCollections), "EX", CACHE_KEEP_TIME_S)
+      firstEntrySet = true
+    } else if (numCollections > MAX_COLLECTIONS_PER_CACHE_REQUEST) {
+
+      redis.set("cache", "[", "EX", CACHE_KEEP_TIME_S)
+      let remainder = numCollections
+      let offset = 0
+      let toCache = []
+      while (remainder > 0) {
+        let offsetEnd = offset + Math.min(remainder, MAX_COLLECTIONS_PER_CACHE_REQUEST)
+        toCache = JSON.stringify(rawCollections.slice(offset, offsetEnd)).slice(1,-1)
+
+        toCache = offset == 0 ? toCache : `,${toCache}`
+
+        remainder -= MAX_COLLECTIONS_PER_CACHE_REQUEST
+        offset += MAX_COLLECTIONS_PER_CACHE_REQUEST
+      }
+
+      redis.append("cache", "]")
+    }
   }
   const collections = result.data
-  // const collections = result.data.filter(c => c.stats.total_volume > 0)
+
   let returnObj = {
     allCollections: addAdditionalAttributes(collections)
   }
