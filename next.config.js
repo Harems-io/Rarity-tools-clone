16ae0cbe29848f031bb0b28055b6c64ba9cc523a
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-08-21 14:58:08 -0700

    don't need the sassoption

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'styles')],
+    // includePaths: [path.join(__dirname, 'styles')],
   },
   experimental: {
     staticPageGenerationTimeout: 300
